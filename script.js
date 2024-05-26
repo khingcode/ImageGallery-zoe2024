84473cf757ba62995086d35a61ea64cad3480df2
khingcode <khingissupreme@gmail.com>
2024-05-27 02:55:33 +0800

    added the main stuff for now

---
+++ script.js
@@ -0,0 +1,45 @@
+function togglePopup(){
+    document.getElementById("Rhoyd").classList.toggle("active");
+}
+function togglePopup1(){
+    document.getElementById("Yanna").classList.toggle("active");
+}
+function togglePopup2(){
+    document.getElementById("Yves").classList.toggle("active");
+}
+function togglePopup3(){
+    document.getElementById("Yumi").classList.toggle("active");
+}
+function togglePopup4(){
+    document.getElementById("Soph").classList.toggle("active");
+}
+function togglePopup5(){
+    document.getElementById("Shiji").classList.toggle("active");
+}
+function togglePopup6(){
+    document.getElementById("Raf").classList.toggle("active");
+}
+function togglePopup7(){
+    document.getElementById("Cart").classList.toggle("active");
+}
+function togglePopup8(){
+    document.getElementById("Kahil").classList.toggle("active");
+}
+function togglePopup9(){
+    document.getElementById("Amee").classList.toggle("active");
+}
+function togglePopup10(){
+    document.getElementById("Khing").classList.toggle("active");
+}
+function togglePopup11(){
+    document.getElementById("Earl").classList.toggle("active");
+}
+function togglePopup12(){
+    document.getElementById("Deej").classList.toggle("active");
+}
+function togglePopup13(){
+    document.getElementById("Rhoyd").classList.toggle("active");
+}
+function togglePopup14(){
+    document.getElementById("Rhoyd").classList.toggle("active");
+}
